057e3a6b9bacad7e9496854f5cfe254b5eead8af
Lemmy <10430540+TierynnB@users.noreply.github.com>
2024-04-03 09:41:52 +1000

    multiplied out values

--- src/search.cpp
+++ src/search.cpp
@@ -432,13 +432,10 @@ void Search::Worker::iterative_deepening() {
         {
             int nodesEffort = rootMoves[0].effort * 100 / std::max(size_t(1), size_t(nodes));
 
-            double fallingEval = 0.1067
-                               + 0.0223 * (mainThread->bestPreviousAverageScore - bestValue)
-                               + 0.0097 * (mainThread->iterValue[iterIdx] - bestValue);
-            if (fallingEval < 0.58 || fallingEval > 1.667)
-            {
-                fallingEval = std::clamp(fallingEval, 0.580, 1.667);
-            }
+            double fallingEval = 0.1067 + 0.0223 * mainThread->bestPreviousAverageScore
+                               + 0.0097 * mainThread->iterValue[iterIdx] - 0.032 * bestValue;
+
+            fallingEval = std::clamp(fallingEval, 0.580, 1.667);
             // If the bestMove is stable over several iterations, reduce time accordingly
             timeReduction    = lastBestMoveDepth + 8 < completedDepth ? 1.495 : 0.687;
             double reduction = (1.48 + mainThread->previousTimeReduction) / (2.17 * timeReduction);
