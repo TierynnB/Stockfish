512a6dca85abc108f26ee188e2cf76553a9288be
Lemmy <10430540+TierynnB@users.noreply.github.com>
2024-07-01 05:38:39 +1000

    included positive limit to optimism, and 50rule count

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -46,8 +46,14 @@ int Eval::simple_eval(const Position& pos, Color c) {
 }
 
 bool Eval::use_smallnet(const Position& pos, int optimism) {
+
     int simpleEval = simple_eval(pos, pos.side_to_move());
-    return std::abs(simpleEval) > (962 + 50 * (optimism < -119));
+
+    // make it harder or easier to use smallnet based on optimism.
+    int addToThreshold = optimism < -119 ? 50 : optimism > 110 ? -20 : 0;
+    //
+    addToThreshold += pos.rule50_count() > 13 ? 100 : 0;
+    return std::abs(simpleEval) > 962 + addToThreshold;
 }
 
 // Evaluate is the evaluator for the outer world. It returns a static evaluation
