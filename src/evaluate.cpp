7fa2980db3ef9f0c40090c9d55f56f25bc73bd5c
Lemmy <10430540+TierynnB@users.noreply.github.com>
2024-07-01 17:44:18 +1000

    use material in smallnet decision

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -47,7 +47,8 @@ int Eval::simple_eval(const Position& pos, Color c) {
 
 bool Eval::use_smallnet(const Position& pos) {
     int simpleEval = simple_eval(pos, pos.side_to_move());
-    return std::abs(simpleEval) > 962;
+    int material   = 554 * pos.count<PAWN>() + pos.non_pawn_material();
+    return std::abs(simpleEval) > 962 || (material < 1900 && std::abs(simpleEval) > 700);
 }
 
 // Evaluate is the evaluator for the outer world. It returns a static evaluation
